18a348c41cfbd2df611e6cdeb519db322fa07bd3
Ninh Manh Hung <manhhungninh@gmail.com>
2019-07-26 17:21:03 +0700

    add new session

---
+++ session2_var/my_var.js
@@ -0,0 +1,72 @@
+// let X = 5;
+// let y = "Hello World";
+// let Y = "Welcome";
+
+// const pi = 3.14;
+
+// let xinChao = 'Xin Chao';
+// let XinChao = 'Chao cac ban';
+
+// var welcome = "Welcome to C4E JS 41";
+
+// console.log(pi);
+// // console.log(xinChao);
+// // console.log(XinChao);
+
+// console.log(X);
+// console.log(y);
+// console.log(welcome);
+// // pi = 5;
+// const pi = 3.14;
+
+// let X = 5;
+// console.log(X);
+// X = 10;
+// console.log(X);
+
+// var y = 20;
+
+// var z = X + y;
+// var i = X * y;
+// var a = y / X;
+// var b = y % X;
+
+// console.log("Gia tri" + "cua z la:" + z); // + : cộng chuỗi
+// console.log("Gia tri cua i la: " + i);
+// console.log(a);
+// console.log(b);
+
+//pham vi bien
+// var x = 50;
+// let m = 100;
+// {
+//     let x = 20;
+//    // console.log(x);
+//     // var y = "10";
+//     // console.log(y);
+//     let m = 10;
+//     console.log(m);
+// }
+
+// //console.log(x);
+// // console.log(y);
+// console.log(m);
+
+// let x = 100;
+// {
+//     var x = 10;
+//     console.log(x);
+    
+// }
+// console.log(x);
+// //=>
+// var x;
+// let x = 100;
+// {
+//     x = 10;
+// }
+
+x = 100;
+console.log(x);
+var x;
+
---
+++ session3_datatypes/datatypes_intro.js
@@ -0,0 +1,45 @@
+// var x = 100;
+
+// // let y = "100";
+// // console.log(x);
+// var x = 100;
+// console.log(x++);
+// console.log(x);
+
+// var x = 100;
+// console.log(++x);
+// console.log(x);
+
+// var x = 100;
+// console.log(--x);
+// var x = 100;
+// console.log(x--);
+
+
+
+// // console.log(y);
+// // console.log(`Gia tri cua x la: ${x}`);
+
+// // let a = 10;
+// // let b = 20;
+// // console.log(`Tong cua ${a} va ${b} la ${a + b}`);
+// const year = 2019;
+// let birth = prompt("Ban sinh nam bao nhieu?");
+// console.log(year - birth);
+// let name = prompt("Ban ten la gi?")
+// console.log(`Chao ban ${name}, ban ${year - birth} tuoi`);
+
+// BAI1
+// var add = prompt("Ban ten la gi va bao nhieu tuoi?(Nhap theo thu tu va cach nhau dau ,)");
+// var info = add.split(",");
+// console.log(`Chao ban ${info[0]}, ban ${info[1]} tuoi`);
+
+//BAI2
+const year = 2019;
+var add = prompt("Ban ten la gi va sinh nam bao nhieu?(Nhap theo thu tu va cach nhau dau ,)");
+var info = add.split(",");
+console.log(`Chao ban ${info[0]}, ban ${year - info[1]} tuoi`);
+
+
+
+
